c9c417fca333e4c959eae7813a1fada8ddb420c1
StephanJW <stephanjwlodarczyk@gmail.com>
2013-01-11 17:54:23 -0500

    Update src/Employee.java
    Added getters and setters for employee number and team number and made modifications
    to 'goTo' methods to incorporate messages describing employee actions.

--- src/Employee.java
+++ src/Employee.java
@@ -1,14 +1,113 @@
+import java.util.Random;
+
 
 public abstract class Employee extends Thread {
 
+	private int employNumber;
+	private int teamNumber;
+	
 	private boolean idle;
 	private boolean eatenLunch;
 
+	private Random randomNum = new Random();
+	
+	private int arrivalTime;
+	private int lunchTime;
+	private long simulationTime;
+	
+	/**
+	 * Sets a number to reference the individual employee
+	 * @param employNumber
+	 */
+	public void setEmployeeNumber(int employNumber) {
+		this.employNumber = employNumber;
+	}
+	
+	/**
+	 * Gets a specified number assigned to each employee
+	 * @return employee reference number
+	 */
+	public int getEmployNumber() {
+		return employNumber;
+	}
+	
+	/**
+	 * Sets the team the employee is assigned to
+	 * @param teamNumber
+	 */
+	public void setTeamNumber(int teamNumber) {
+		this.teamNumber = teamNumber;
+	}
+	
+	/**
+	 * Gets a specified team number assigned to each employee
+	 * @return employee team number
+	 */
+	public int getTeamNumber() {		
+		return teamNumber;
+	}	
 
-	private void goToWork();
-	private void goHome();
-	private void goToLunch();
-	private void returnFromLunch();
-	private void returnFromBusy();
+	/**
+	 * Specifies an employee arriving to the work place
+	 */
+	private void goToWork() {
+
+		idle = false;
+
+		// generate a time between 0 and 30 minutes
+		arrivalTime = randomNum.nextInt(30);
+		
+		// convert arrival time to simulation time
+		simulationTime = arrivalTime * 10;		
+
+		try {
+			sleep(simulationTime);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Employee " + employNumber + " on team " + getTeamNumber() + " arrived to work.");
+
+		returnToWork();
+	}
+
+	/**
+	 * Specifies an employee leaving the work place
+	 */
+	private void goHome() {
+		System.out.println("Employee " + employNumber + " on team " + getTeamNumber() + " has left the work place.");		
+	}
+
+	/**
+	 * Specifies an employee taking their lunch break
+	 */
+	private void goToLunch() {
+
+		idle = false;
+		eatenLunch = true;
+
+		// generate a time between 0 and 60 - time arrived to work
+		lunchTime = randomNum.nextInt(60-arrivalTime);
+		
+		// convert lunch time to simulation time
+		simulationTime = lunchTime * 10;	
+
+		try {
+			sleep(simulationTime);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Employee " + employNumber + " on team " + getTeamNumber() + " went to lunch.");
+
+		returnToWork();
+	}
+
+	/**
+	 * Set the state to an "idle" working state
+	 */
+	private void returnToWork() {		
+		System.out.println("Employee " + employNumber + " on team " + getTeamNumber() + " is now working.");		
+		idle = true;
+	}
 }
-
