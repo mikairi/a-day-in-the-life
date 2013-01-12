373096e3268176528297e4670bffb0eeba472f62
Wlodarczyk <Wlodarczyk@NASA-HDX-16>
2013-01-12 13:01:19 -0500

    Add logic to developer stubbed methods

--- src/Developer.java
+++ src/Developer.java
@@ -14,6 +14,15 @@ public class Developer extends Employee {
 	*	Ask a question to your team lead
 	*/
 	public void askTeamLeadQuestion(){
+		System.out.println("TIME Developer " + getTeamNumber() + getEmployNumber() + " asks a question.");
+		
+		// 10 minutes to propose a question
+		try {
+			sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		myLead.answerTeamQuestion();
 	}
 	
@@ -21,8 +30,24 @@ public class Developer extends Employee {
 	*	Answer a question from your team lead
 	*/
 	public void answerTeamLeadQuestion(){
+		System.out.println("TIME Team leader asks developer " + getTeamNumber() + getEmployNumber() + " a questions");
 	
-	
+		// 10 minutes to answer a question
+		try {
+			sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		Random randGen = new Random();
+		// If false, team lead and the member go to ask the manager;
+		// otherwise answer and naturally return to work.
+		if (randGen.nextBoolean()) {
+			myLead.askManagerQuestion();
+		} else {
+			System.out.println("TIME Developer " + getTeamNumber() + getEmployNumber() + " answered their team lead's question.");
+			goToWork();
+		}
 	}
 	
 	// meeting with other devs and team lead
