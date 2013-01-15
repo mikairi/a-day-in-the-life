f99c8f466a8b8d196c7bbb7559b2fb0db4cb67a4
Wlodarczyk <Wlodarczyk@129.21.137.112>
2013-01-14 19:14:27 -0500

    Modified manager.java to work on functionality within stand-up meeting and providing answers to questions.

--- src/Manager.java
+++ src/Manager.java
@@ -8,13 +8,30 @@ public class Manager extends Employee {
 	*	Answer a question from your team lead
 	*/
 	public void answerTeamLeadQuestion(){
-	
-	
+		System.out.println("TIME Team leader asks Manager a question.");
+		
+		// 10 minutes to answer a question
+		try {
+			sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}		
+
+		System.out.println("TIME Manager provides an answer to team leader's question.");	
 	}
 
 	// meeting with team leads
 	public void startStandUp(){
-	
+		
+		// Manager begins morning stand-up meeting
+		System.out.println("TIME Manager and team leaders are engaged in this morning's stand-up meeting.");
+		
+		// 15 minutes for stand-up meeting
+		try {
+			sleep(150);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	void goToWork() {
