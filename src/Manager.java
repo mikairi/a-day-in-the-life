77d277ba72f0d614ac29c97f6360e66446a8f936
Kai <kch2100@rit.edu>
2013-01-16 19:03:50 -0500

    More cleanup.

--- src/Manager.java
+++ src/Manager.java
@@ -12,46 +12,42 @@ public class Manager extends Employee {
 		super(firm);
 		setName("Manager");
 	}
-	
+
 	public synchronized void addTeamLeader(TeamLeader lead) {
 		myLeads.add(lead);
 	}
-	
+
 	public boolean getIsManagerBusy() {
 		return isBusy;
 	}
-	
-	public CyclicBarrier getMorningTeamLeadStandup()
-	{
+
+	public CyclicBarrier getMorningTeamLeadStandup() {
 		return morningTeamLeadStandup;
 	}
 
-	
 	/**
-	*	Answer a question from your team lead
-	*/
-	public void answerTeamLeadQuestion(){
-	// 10 minutes to answer a question
-			try {
-				sleep(100);
-				logAction("answered Team Leader question.");
-			}	 
-			catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+	 * Answer a question from your team lead
+	 */
+	public void answerTeamLeadQuestion() {
+		// 10 minutes to answer a question
+		try {
+			sleep(100);
+			logAction("answered Team Leader question.");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	// meeting with team leads
 	public void startStandUp(){
 		// 15 minutes for stand-up meeting 
 		try {
-			System.out.println("TIME " + Thread.currentThread().getName() + " engages in morning stand-up meeting with TeamLeads.");
+			logAction("engages in morning stand-up meeting with Team Leads.");
 			sleep(150);
-			System.out.println("TIME " + Thread.currentThread().getName() + " concludes the morning stand-up meeting with TeamLeads.");
+			logAction("concludes the morning stand-up meeting with Team Leads.");
 		} 
 		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 }
-
